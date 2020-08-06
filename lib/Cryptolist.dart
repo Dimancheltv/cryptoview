bd16b8640c2c882c1eae3cd7426f5dabf9204cf4
Brave_Engineer <the.polyhedral@gmail.com>
2020-08-06 14:32:09 +0300

    api data dispaly

--- lib/Cryptolist.dart
+++ lib/Cryptolist.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 import 'CryptoData.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class CryptoList extends StatelessWidget{
+class CryptoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CryptoListState();
+  }
+}
 
-  List<CryptoData> data = [
-    CryptoData(name: 'Bitcoin', symbol: 'BTC', rank: 1, price: 12000.0),
-    CryptoData(name: 'Etherium', symbol: 'ETH', rank: 2, price: 200.0)
-  ];
+class CryptoListState extends State<CryptoList> {
+  List<CryptoData> data = [];
+
+  _loadCurrency() async {
+    final response = await http.get('https://api.coincap.io/v2/assets?limit=10');
+    if (response.statusCode == 200) {
+      var allData = (json.decode(response.body) as Map)['data'];
+      var dataList = List<CryptoData>();
+      allData.forEach((val) {
+        var record = CryptoData(
+            name: val['name'],
+            symbol: val['symbol'],
+            price: double.parse(val['priceUsd']),
+            rank: int.parse(val['rank']));
+        dataList.add(record);
+      });
+      setState(() {
+        data = dataList;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,16 +43,28 @@ class CryptoList extends StatelessWidget{
           children: _buildList(),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.refresh),
+        onPressed: () => _loadCurrency(),
+      ),
     );
   }
-  List<Widget> _buildList(){
-    return data.map((CryptoData f) => ListTile(
-      title: Text(f.name),
-      subtitle: Text(f.symbol),
-      leading: CircleAvatar(
-        child: Text(f.rank.toString())),
-      trailing: Text('\$${f.price.toString()}'),
-    )).toList();
-  }
-}
 
+  List<Widget> _buildList() {
+    return data
+        .map((CryptoData f) => ListTile(
+              title: Text(f.name),
+              subtitle: Text(f.symbol),
+              leading: CircleAvatar(child: Text(f.rank.toString())),
+              trailing: Text('\$${f.price.toStringAsFixed(2)}'),
+            ))
+        .toList();
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _loadCurrency();
+  }
+
+}
